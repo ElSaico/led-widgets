ca3f881c77797a805348430f216081de0b300876
Bruno Marques <bruno.oliveira_marques@siemens.com>
2023-02-02 15:20:22 -0300

    Made alert box dumber (moving most logic to Streamer.bot)

--- alerts.js
+++ alerts.js
@@ -1,3 +1,4 @@
+import './node_modules/modern-async/dist/modern-async.umd.js';
 import { $Font } from './node_modules/bdfparser/dist/esm/bdfparser.js';
 import fetchline from './node_modules/fetchline/dist/esm/index.js';
 
@@ -10,23 +11,11 @@ function connect() {
   return new WebSocket("ws://localhost:8080/");
 }
 
-function displayUser(name, login, anonymous) {
-  if (anonymous) return 'anônimo';
-  return /\p{ASCII}+/u.test(name) ? name : login;
-}
-
-function pluralize(amount, singular, plural) {
-  return amount === 1 ? `${amount} ${singular}` : `${amount} ${plural}`;
-}
-
-function latinize(text) {
-  return text.replaceAll('\u2212', '-');
-}
-
 document.addEventListener('DOMContentLoaded', async () => {
   const fontIbm = await $Font(fetchline('fonts/ibm8x8.bdf'));
   const fontMetro = await $Font(fetchline('fonts/metro.bdf'));
   const panel = new LEDPanel(document.getElementById('panel'), {x: 200, y: 8});
+  const queue = new modernAsync.Queue(1);
 
   let ws = connect();
   ws.onopen = () => {
@@ -41,53 +30,33 @@ document.addEventListener('DOMContentLoaded', async () => {
   ws.onclose = () => {
     setTimeout(() => { ws = connect; }, 10000);
   };
-  ws.onmessage = async event => {
+  ws.onmessage = event => {
     const data = JSON.parse(event.data);
     if (data.status) return;
 
-    // TODO counters
-    // TODO shoutout
-    const args = data.data.arguments;
-    let title, game;
-    let userName = displayUser(args.user, args.userName, args.anonymous);
-    const message = args.message;
-    switch (args.actionName) {
-      case 'Follow':
-        title = 'Novo passageiro no Comboio';
+    const message = data.data.arguments.alertMessage;
+    if (!message) return;
+    switch (data.data.name) {
+      case 'Alert trigger: Fixed':
+        queue.exec(async () => {
+          await panel.drawLoopable(fontIbm, message, COLOR_IBM, 25);
+        });
         break;
-      case 'Bits':
-      case 'Bits: Anônimo':
-        title = `${pluralize(args.bits, 'bit enviado', 'bits enviados')} para o Comboio`;
+      case 'Alert trigger: Scroll':
+        queue.exec(async () => {
+          await panel.drawScroll(fontIbm, message, COLOR_IBM, 25);
+        });
         break;
-      case 'Raid':
-        title = `Embarque de uma raid com ${pluralize(args.viewers, 'pessoa', 'pessoas')}`;
+      case 'Alert trigger: Animated H':
+        queue.exec(async () => {
+          await panel.drawAnimatedHorizontal(fontMetro, message, COLOR_METRO, 50);
+        });
         break;
-      case 'Sub':
-        title = 'Passe adquirido pela primeira vez';
+      case 'Alert trigger: Animated V':
+        queue.exec(async () => {
+          await panel.drawAnimatedVertical(fontMetro, message, COLOR_METRO, 250);
+        });
         break;
-      case 'ReSub':
-        title = `Passe renovado, somando ${args.cumulative} meses`;
-        break;
-      case 'Sub: Gift':
-        title = `Passe doado por ${userName}`;
-        userName = displayUser(args.recipientUser, args.recipientUserName, false);
-        break;
-      case 'Sub: Bomb':
-        title = `Doação de ${args.gifts} passes simultâneos`;
-        break;
-    }
-    if (title) {
-      // TODO can we call TTS from here?
-      await panel.drawScroll(fontIbm, title, COLOR_IBM, 25);
-    }
-    if (userName) {
-      await panel.drawAnimatedVertical(fontMetro, userName, COLOR_METRO, 250);
-    }
-    if (game) {
-      await panel.drawAnimatedHorizontal(fontMetro, game, COLOR_METRO, 50);
-    }
-    if (message) {
-      await panel.drawScroll(fontIbm, message, COLOR_IBM, 25);
     }
   };
 });
